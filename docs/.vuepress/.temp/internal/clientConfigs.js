1e0b484daad22158fb4982cca8751d4ec91814cb
Linncharm <linncharm9@gmail.com>
2024-11-17 20:53:04 +0800

    modified the deploy.yml, first try

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,25 @@
+import * as clientConfig0 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@_ak2w3zzxzv633ckcbud6m4nhha/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_r2yupepkytdnq5ulznqxnts4am/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_wahdeyzsafbbhzffe3p6vfevri/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.60_markdown-it@14.1.0_vue@3.5.13_vuepress@2.0.0-rc.18_ty6q6nbsuhcyuwidrrc5hpja4a/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_x5d23jevrv7j5s5srkdqefr7du/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_zoxh3mjsbricfcscwgqllidapi/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from 'D:/vueDev/MyVuePressProject/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from 'D:/vueDev/MyVuePressProject/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.60_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc._7mq2x2dd6oklulaoqwx33iovty/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from 'D:/vueDev/MyVuePressProject/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.60_markdown-it@14.1.0_sass@1.81.0_vuepress@2.0.0-rc.18_@vuep_vflqzg7f2unj5rirf774uyigki/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig10 from 'D:/vueDev/MyVuePressProject/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+].map((m) => m.default).filter(Boolean)
